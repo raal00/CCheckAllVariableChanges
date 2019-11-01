6b2d3f7551726464fc754a3d215a679587993b73
Raal00 <ivan.pyzhyznov.90@mail.ru>
2019-11-01 20:44:02 +0300

    a

--- KR_debugC/mainSource.c
+++ KR_debugC/mainSource.c
@@ -3,29 +3,42 @@
 #include <locale.h>
 #include <stdbool.h>
 
-const char* TestCFilePath = "test.c";
-const char* LogFile = "log.txt";
+const char* TestCFilePath = "D:/Программирование/backend/C/ByteC/ByteC/Source.c";
 
 int  GetLineLength(char* Line);
 int  VariableContains(char* Line, int LineLength, int StartIndex, char* SubString, int SubStringLength);
-void FindAllVariableChanges(char* Line, char** variablesArray, int variablesArrayLenght, int lineNumber);
 
 int main() 
 {
-	#pragma region Variables
+	#pragma region variables
 	char**	searchedVariablesArray;
 	int		searchedVariablesArrayLength;
 	int		searchedVariableLength = 20;
+	int		variableLength;
 
 	int		LineNumber = 1;
-	char*	CurrentCodeLine;
-	int		MaxLineLength = 255;
-	FILE*	InputFileThread;
-	char*	locale = setlocale(LC_ALL, "");
+	int		ContainsAtLineResult;
+	int		OperatorIndex;
+	int		VariableFirstIndex;
+	int		index;
 
+	char*	CurrentCodeLine;
+	char*	variable;
+	char*	Operator;
+	char*	locale;
+
+	int		lineLength;
+	int		MaxLineLength = 255;
+	int		MaxOperatorLength = 127;
+
+	bool SearchingOperator = false;
+	bool variableChanged = false;
+	bool VariableFoundAtFunction;
+	FILE*	InputFileThread;
+	
 	#pragma endregion
 
-	// step 1
+	locale = setlocale(LC_ALL, "Russian");
 	if ((InputFileThread = fopen(TestCFilePath, "r")) == NULL)
 	{
 		printf("1) Файл %s не существует\n", TestCFilePath);
@@ -35,9 +48,6 @@ int main()
 	{
 		printf("1) Файл %s получен\n", TestCFilePath);
 	}
-
-	// step 2
-
 	
 	printf("2) Введите количество переменных: ");
 	scanf("%d", &searchedVariablesArrayLength);
@@ -63,21 +73,19 @@ int main()
 	printf("3) Начинаем поиск\n");
 
 	CurrentCodeLine = (char*)malloc(sizeof(char) * MaxLineLength);
-	bool SearchingOperator = false;
-	int OperatorCount = 0;
+	Operator = (char*)malloc(sizeof(char) * MaxOperatorLength);
 	
 	// Search all variables
 	for (int i = 0; i < searchedVariablesArrayLength; i++)
 	{
-		char* variable = searchedVariablesArray[i];
+		variable = searchedVariablesArray[i];
 		printf("%d) %s\n", i, variable);
-		int	  variableLength = GetLineLength(variable);
-		int LBoard = 0;
+		variableLength = GetLineLength(variable);
 
 		while (fgets(CurrentCodeLine, MaxLineLength, InputFileThread) != NULL)
 		{
-			int index = 0;
-			int lineLength = GetLineLength(CurrentCodeLine);
+			index = 0;
+			lineLength = GetLineLength(CurrentCodeLine);
 
 			while (index < lineLength) 
 			{
@@ -85,10 +93,32 @@ int main()
 				{
 					while (1)
 					{
-						if (index == lineLength) break;
-						if (CurrentCodeLine[index] != ' ' && CurrentCodeLine[index] != '\t') printf("%c",CurrentCodeLine[index]);
+						if (index == lineLength) 
+						{
+							break; 
+						}
+						if (CurrentCodeLine[index] != ' ' && CurrentCodeLine[index] != '\t')
+						{
+							Operator[OperatorIndex] = CurrentCodeLine[index];
+							OperatorIndex++;
+						}
+						if (CurrentCodeLine[index] == '+' || CurrentCodeLine[index] == '-' ||
+							CurrentCodeLine[index] == '/' || CurrentCodeLine[index] == '%' ||
+							CurrentCodeLine[index] == '*' || 
+								(CurrentCodeLine[index] == '=' && CurrentCodeLine[index-1] != '=' && 
+								 CurrentCodeLine[index + 1] != '=' && CurrentCodeLine[index - 1] != '!'))
+						{
+							variableChanged = true;
+						}
+
 						if (CurrentCodeLine[index] == ';')
 						{
+							if (variableChanged == true) 
+							{
+								Operator[OperatorIndex] = '\0';
+								printf("Operator on line %d: %s %s", LineNumber, variable, Operator);
+							}
+							variableChanged = false;
 							SearchingOperator = false;
 							printf("%c", '\n');
 							break;
@@ -96,16 +126,48 @@ int main()
 						index++;
 					}
 				}
-				if (index == lineLength) break;
-
-				int res = VariableContains(CurrentCodeLine, lineLength, index, variable, variableLength);
-				if (res >= 0)
-				{
-					printf("Operator on line %d: ", LineNumber);
-					index = res + variableLength - 1;
-					SearchingOperator = true;
+				if (index == lineLength) 
+				{ 
+					break; 
 				}
-				else { break; }
+
+				ContainsAtLineResult = VariableContains(CurrentCodeLine, lineLength, index, variable, variableLength);
+				if (ContainsAtLineResult >= 0)
+				{
+					VariableFirstIndex = ContainsAtLineResult;
+					VariableFoundAtFunction = false;
+					while (VariableFirstIndex > 0)
+					{
+						VariableFirstIndex--;
+						if (CurrentCodeLine[VariableFirstIndex] == '=')
+						{
+							VariableFoundAtFunction = true;
+							break;
+						}
+						if (CurrentCodeLine[VariableFirstIndex] == ';') 
+						{ 
+							break; 
+						}
+					}
+					if (VariableFoundAtFunction == false) 
+					{
+						SearchingOperator = true;
+						for (OperatorIndex = 0; OperatorIndex < MaxOperatorLength; OperatorIndex++)
+						{
+							Operator[OperatorIndex] = NULL;
+						}
+						OperatorIndex = 0;
+					}
+					else 
+					{
+						VariableFoundAtFunction = false;
+					}
+					index = ContainsAtLineResult + variableLength;
+				}
+				else 
+				{ 
+					break; 
+				}
 			}
 			LineNumber++;
 		}
@@ -120,7 +182,12 @@ int main()
 
 int  GetLineLength(char* Line)
 {
+	#pragma region  variables
+	
 	int index = 0;
+	
+	#pragma endregion
+
 	while ((Line[index] != NULL) && (Line[index] != '\n') && (Line[index] != '\0') )
 	{
 		index++;
@@ -128,16 +195,22 @@ int  GetLineLength(char* Line)
 	return index;
 }
 
+
+/// <example>
+///		> Line: "int   value1 = 1; val=3; float VALue;" SubString: "value"
+///		< -1
+/// </example>
 int VariableContains(char* Line, int LineLength, int SearchFrom, char* SubString, int SubStringLength)
 {
-	/// <example>
-	///		> Line: "int   value1 = 1; val=3; float VALue;" SubString: "value"
-	///		< not found
-	/// </example>
+	#pragma region  variables
 
 	char	CurrentLetter;
 	int		LineIndex = 0;
+	int firstIndex;
 
+	#pragma endregion
+
+	
 	if (SubStringLength > LineLength - SearchFrom) 
 	{
 		return -2; 
@@ -154,12 +227,12 @@ int VariableContains(char* Line, int LineLength, int SearchFrom, char* SubString
 												  || Line[j - 1] == '\0')) 
 																								
 			{
-				int firstIndex = j;
+				firstIndex = j;
 				do
 				{
 					j++;
 					k++;
-					if (j >= firstIndex + SubStringLength)
+					if (j == firstIndex + SubStringLength - 1)
 					{
 						// whole word found -> check next symb
 						if (Line[j + 1] == NULL || Line[j + 1] == '\0' || Line[j + 1] == ' '
