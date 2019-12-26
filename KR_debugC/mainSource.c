81b11c781fd755a3b642f7577cdb46310e540dc1
Raal00 <ivan.pyzhyznov.90@mail.ru>
2019-12-26 12:57:59 +0300

    aa

--- KR_debugC/mainSource.c
+++ KR_debugC/mainSource.c
@@ -1,16 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
+#include <string.h>
 #include <stdbool.h>
 
-const char* TestCFilePath = "D:/Программирование/backend/C/ByteC/ByteC/Source.c";
+const char* TestCFilePath = "test.c";
+
+typedef struct
+{	
+	char* VarName;
+	int   Length;
+}Var;
 
 int  GetLineLength(char* Line);
+int  getCountOfVariables(char* CFilePath);
 int  VariableContains(char* Line, int LineLength, int StartIndex, char* SubString, int SubStringLength);
 
 int main() 
 {
-	#pragma region variables
 	char**	searchedVariablesArray;
 	int		searchedVariablesArrayLength;
 	int		searchedVariableLength = 20;
@@ -21,67 +29,117 @@ int main()
 	int		OperatorIndex;
 	int		VariableFirstIndex;
 	int		index;
+	int		prevIndex;
+	int		i;
+	int		variablesCount;
 
 	char*	CurrentCodeLine;
 	char*	variable;
 	char*	Operator;
 	char*	locale;
+	char	LastSymbAtOperator;
 
 	int		lineLength;
-	int		MaxLineLength = 255;
-	int		MaxOperatorLength = 127;
+	int		MaxLineLength = 256;
+	int		MaxOperatorLength = 64;
 
 	bool SearchingOperator = false;
 	bool variableChanged = false;
 	bool VariableFoundAtFunction;
+	bool decPrev = false;
+	bool incPrev = false;
 	FILE*	InputFileThread;
-	
-	#pragma endregion
 
 	locale = setlocale(LC_ALL, "Russian");
+
 	if ((InputFileThread = fopen(TestCFilePath, "r")) == NULL)
 	{
 		printf("1) Файл %s не существует\n", TestCFilePath);
-		return 1;
+		return -7;
 	}
 	else
 	{
 		printf("1) Файл %s получен\n", TestCFilePath);
 	}
-	
-	printf("2) Введите количество переменных: ");
-	scanf("%d", &searchedVariablesArrayLength);
-	
-	if (searchedVariablesArrayLength <= 0) 
+
+	variablesCount = getCountOfVariables(TestCFilePath);
+	if (variablesCount > 0) 
 	{
-		printf("3) Число искомых переменных должно быть >0\n");
+		printf("Найдено переменных в файле: %d\n", variablesCount);
 	}
 	else 
 	{
-		printf("\n******************************************\n");
+		printf("В тексте программы не найдено переменных\n");
+		return -3;
 	}
 
-	searchedVariablesArray = (char**)malloc(sizeof(void *) * searchedVariablesArrayLength); // {val1, val2...}
+	printf("2) Введите количество переменных: ");
 
-	for (int i = 0; i < searchedVariablesArrayLength; i++) 
+	if (scanf("%d", &searchedVariablesArrayLength) != 1)
+	{
+		printf("Значение количества переменных должно быть целое число");
+		return -1;
+	}
+	rewind(stdin);
+
+	if (searchedVariablesArrayLength <= 0)
+	{
+		printf("3) Число искомых переменных должно быть >0\n");
+		return -2;
+	}
+	else if (searchedVariablesArrayLength > variablesCount)
+	{
+		printf("Вы ввели слишком большое количество переменных! Максимальное значение = %d\n", variablesCount);
+		searchedVariablesArrayLength = variablesCount;
+	}
+	else
+	{
+		printf("\n******************************************\n");
+	}
+	searchedVariablesArray = (char**)malloc(sizeof(void *) * searchedVariablesArrayLength);
+	if (searchedVariablesArray == NULL) 
+	{
+		printf("Не удалось выделить память под имена переменных\n");
+		return -3;
+	}
+	for (i = 0; i < searchedVariablesArrayLength; i++) 
 	{
 		searchedVariablesArray[i] = (char*)malloc(sizeof(char) * searchedVariableLength);
+		if (searchedVariablesArray[i] == NULL) 
+		{
+			printf("Не удалось выделить память под имя переменной\n");
+			return -4;
+		}
 		printf("%d. Введите имя переменной: ", i);
-		scanf("%s", searchedVariablesArray[i]);
+		if (scanf("%s", searchedVariablesArray[i]) != 1) 
+		{
+			printf("Не удалось считать имя переменной\n");
+			return -8;
+		}
+		rewind(stdin);
 		printf("\n");
 	}
 	printf("3) Начинаем поиск\n");
 
 	CurrentCodeLine = (char*)malloc(sizeof(char) * MaxLineLength);
 	Operator = (char*)malloc(sizeof(char) * MaxOperatorLength);
-	
+	if (CurrentCodeLine == NULL) 
+	{
+		printf("Не удалось выделить память под строку из файла\n");
+		return -5;
+	}
+	if (Operator == NULL)
+	{
+		printf("Не удалось выделить память под строку оператора\n");
+		return -6;
+	}
 	// Search all variables
-	for (int i = 0; i < searchedVariablesArrayLength; i++)
+	for (i = 0; i < searchedVariablesArrayLength; i++)
 	{
 		variable = searchedVariablesArray[i];
-		printf("%d) %s\n", i, variable);
+		printf("%d) %s:\n", i, variable);
 		variableLength = GetLineLength(variable);
-
+		LineNumber = 1;
 		while (fgets(CurrentCodeLine, MaxLineLength, InputFileThread) != NULL)
 		{
 			index = 0;
@@ -97,32 +155,32 @@ int main()
 						{
 							break; 
 						}
-						if (CurrentCodeLine[index] != ' ' && CurrentCodeLine[index] != '\t')
-						{
-							Operator[OperatorIndex] = CurrentCodeLine[index];
-							OperatorIndex++;
-						}
 						if (CurrentCodeLine[index] == '+' || CurrentCodeLine[index] == '-' ||
 							CurrentCodeLine[index] == '/' || CurrentCodeLine[index] == '%' ||
-							CurrentCodeLine[index] == '*' || 
-								(CurrentCodeLine[index] == '=' && CurrentCodeLine[index-1] != '=' && 
-								 CurrentCodeLine[index + 1] != '=' && CurrentCodeLine[index - 1] != '!'))
+							CurrentCodeLine[index] == '*' || CurrentCodeLine[index] == '^' ||
+							CurrentCodeLine[index] == '|' || CurrentCodeLine[index] == '&' ||
+							(CurrentCodeLine[index] == '=' && CurrentCodeLine[index - 1] != '=' &&
+								CurrentCodeLine[index + 1] != '=' && CurrentCodeLine[index - 1] != '!'))
 						{
 							variableChanged = true;
 						}
-
-						if (CurrentCodeLine[index] == ';')
+						if (CurrentCodeLine[index] == LastSymbAtOperator || CurrentCodeLine[index] == '\0')
 						{
 							if (variableChanged == true) 
 							{
 								Operator[OperatorIndex] = '\0';
-								printf("Operator on line %d: %s %s", LineNumber, variable, Operator);
+								if (VariableFoundAtFunction == true)
+								printf("Operator on line %d: %s%c\n", LineNumber, Operator, LastSymbAtOperator);
+								else 
+								printf("Operator on line %d: %s%s%c\n", LineNumber, variable ,Operator, LastSymbAtOperator);
 							}
+							OperatorIndex = 0;
 							variableChanged = false;
 							SearchingOperator = false;
-							printf("%c", '\n');
 							break;
 						}
+						Operator[OperatorIndex] = CurrentCodeLine[index];
+						OperatorIndex++;
 						index++;
 					}
 				}
@@ -130,39 +188,89 @@ int main()
 				{ 
 					break; 
 				}
-
 				ContainsAtLineResult = VariableContains(CurrentCodeLine, lineLength, index, variable, variableLength);
 				if (ContainsAtLineResult >= 0)
 				{
-					VariableFirstIndex = ContainsAtLineResult;
-					VariableFoundAtFunction = false;
-					while (VariableFirstIndex > 0)
+					if (ContainsAtLineResult >= 2)
 					{
-						VariableFirstIndex--;
-						if (CurrentCodeLine[VariableFirstIndex] == '=')
+						prevIndex = ContainsAtLineResult;
+						while (prevIndex > 0)
 						{
-							VariableFoundAtFunction = true;
-							break;
+							if (CurrentCodeLine[prevIndex] == '-')
+							{
+								if (decPrev)
+								{
+									printf("Operator on line %d: %s%s;\n", LineNumber, "--", variable);
+									index = ContainsAtLineResult + variableLength;
+									break;
+								}
+								else 
+								{ 
+									decPrev = true; 
+								}
+							}
+							if (CurrentCodeLine[prevIndex] == '+')
+							{
+								if (incPrev)
+								{
+									printf("Operator on line %d: %s%s;\n", LineNumber, "++", variable);
+									index = ContainsAtLineResult + variableLength;
+									break;
+								}
+								else 
+								{ 
+									incPrev = true; 
+								}
+							} 
+							else if (CurrentCodeLine[prevIndex] == ';') 
+							{
+								index = ContainsAtLineResult + variableLength;
+								break;
+							}
+
+							prevIndex--;
 						}
-						if (CurrentCodeLine[VariableFirstIndex] == ';') 
-						{ 
-							break; 
-						}
+
 					}
-					if (VariableFoundAtFunction == false) 
+					if (!decPrev && !incPrev) 
 					{
+						VariableFirstIndex = ContainsAtLineResult;
+						VariableFoundAtFunction = false;
+						while (VariableFirstIndex > 0)
+						{
+							VariableFirstIndex--;
+							if (CurrentCodeLine[VariableFirstIndex] == '(')
+							{
+								VariableFoundAtFunction = true;
+								break;
+							}
+							if (CurrentCodeLine[VariableFirstIndex] == ';')
+							{
+								break;
+							}
+						}
+						if (VariableFoundAtFunction)
+						{
+							while (index > 0 && CurrentCodeLine[index-1] != ';')
+							{
+								index--;
+							}
+						}
+						else
+						{
+							index = ContainsAtLineResult + variableLength;
+						}
+						LastSymbAtOperator = ';';
 						SearchingOperator = true;
 						for (OperatorIndex = 0; OperatorIndex < MaxOperatorLength; OperatorIndex++)
 						{
-							Operator[OperatorIndex] = NULL;
+							Operator[OperatorIndex] = '\0';
 						}
 						OperatorIndex = 0;
+						
 					}
-					else 
-					{
-						VariableFoundAtFunction = false;
-					}
-					index = ContainsAtLineResult + variableLength;
+					decPrev = false;
+					incPrev = false;
 				}
 				else 
 				{ 
@@ -171,24 +279,23 @@ int main()
 			}
 			LineNumber++;
 		}
-
-		rewind(InputFileThread);
+		if (fseek(InputFileThread, 0, SEEK_SET) != 0) 
+		{
+			printf("Не удалось перейти в начало файла\n");
+			return -21;
+		}
 	}
 	printf("Finish");
-	getchar();
+	system("pause");
 	return 0;
 }
 
 
 int  GetLineLength(char* Line)
 {
-	#pragma region  variables
-	
 	int index = 0;
-	
-	#pragma endregion
 
-	while ((Line[index] != NULL) && (Line[index] != '\n') && (Line[index] != '\0') )
+	while ((Line[index] != '\n') && (Line[index] != '\0') )
 	{
 		index++;
 	}
@@ -202,47 +309,47 @@ int  GetLineLength(char* Line)
 /// </example>
 int VariableContains(char* Line, int LineLength, int SearchFrom, char* SubString, int SubStringLength)
 {
-	#pragma region  variables
-
 	char	CurrentLetter;
 	int		LineIndex = 0;
-	int firstIndex;
+	int		firstIndex;
+	int		j;
+	int		k;
 
-	#pragma endregion
-
-	
 	if (SubStringLength > LineLength - SearchFrom) 
 	{
 		return -2; 
 	}
 
-	for (int j = SearchFrom; j < LineLength; j++)
+	for (j = SearchFrom; j < LineLength; j++)
 	{
 		CurrentLetter = Line[j];
-		for (int k = 0; k < SubStringLength; k++)
+		for (k = 0; k < SubStringLength; k++)
 		{
 			// first letter found -> check prev symb 
-			if (CurrentLetter == SubString[k] && (   Line[j - 1] == NULL || Line[j - 1] == ' '
+			if (CurrentLetter == SubString[k] && (Line[j - 1] == ' '
 												  || Line[j - 1] == '\n' || Line[j - 1] == '\t'
-												  || Line[j - 1] == '\0')) 
+												  || Line[j - 1] == '\0' || Line[j - 1] == '('
+												  || Line[j - 1] == '+'  || Line[j - 1] == '-'))
 																								
 			{
 				firstIndex = j;
 				do
 				{
-					j++;
-					k++;
 					if (j == firstIndex + SubStringLength - 1)
 					{
 						// whole word found -> check next symb
-						if (Line[j + 1] == NULL || Line[j + 1] == '\0' || Line[j + 1] == ' '
+						if (Line[j + 1] == '\0' || Line[j + 1] == ' '
 							|| Line[j + 1] == '\n' || Line[j + 1] == '\t' || Line[j + 1] == '='
 							|| Line[j + 1] == '%' || Line[j + 1] == '-' || Line[j + 1] == '/'
-							|| Line[j + 1] == '+' || Line[j + 1] == '*' || Line[j + 1] == ';')
+							|| Line[j + 1] == '+' || Line[j + 1] == '*' || Line[j + 1] == ';'
+							|| Line[j + 1] == ')' || Line[j + 1] == ',' || Line[j + 1] == '^'
+							|| Line[j + 1] == '&')
 						{
 							return firstIndex;
 						}
 					}
+					j++;
+					k++;
 				} 
 				while (Line[j] == SubString[k]);
 				
@@ -251,4 +358,131 @@ int VariableContains(char* Line, int LineLength, int SearchFrom, char* SubString
 		}
 	}
 	return -1;
+}
+
+int getCountOfVariables(char* CFilePath) 
+{	
+	char*	CurrentCodeLine;
+
+	int		lineLength;
+	int		MaxLineLength = 255;
+	int		numberOfVar = 0;
+	int		i;
+	int		countOfVariables = 0;
+	int		searchFrom;
+	Var*	types;
+	int		countOfTypes = 9;
+	int		VariablesInline;
+	int		index = 0;
+	int		indexPrev = 0;
+	FILE*	inputThread;
+	bool	isFunctionType;
+	bool	isModVar;
+
+	// <Variable' types>
+	Var INT, SHORT, CHAR, FLOAT, DOUBLE, LONG, UNSIGNED, SIGNED, VOID;
+	INT.VarName		 = "int";
+	SHORT.VarName	 = "short";
+	CHAR.VarName	 = "char";
+	FLOAT.VarName	 = "float";
+	DOUBLE.VarName	 = "double";
+	LONG.VarName	 = "long";
+	UNSIGNED.VarName = "unsigned";
+	SIGNED.VarName	 = "signed";
+	VOID.VarName	 = "void";
+
+	INT.Length		=	strlen(INT.VarName);
+	SHORT.Length	=	strlen(SHORT.VarName);
+	CHAR.Length		=	strlen(CHAR.VarName);
+	FLOAT.Length	=	strlen(FLOAT.VarName);
+	DOUBLE.Length	=	strlen(DOUBLE.VarName);
+	LONG.Length		=	strlen(LONG.VarName);
+	UNSIGNED.Length =	strlen(UNSIGNED.VarName);
+	SIGNED.Length	=	strlen(SIGNED.VarName);
+	VOID.Length		=	strlen(VOID.VarName);
+	// </Variable' types>
+
+	if ((inputThread = fopen(CFilePath, "r")) == NULL)
+	{
+		printf("Файл %s не существует\n", TestCFilePath);
+		return 1;
+	}
+
+	types = malloc(sizeof(Var) * countOfTypes);
+	if (types == NULL)
+	{
+		printf("Не удалось выделить память под массив переменных\n");
+		return -11;
+	}
+	types[0] = INT;		 types[1] = SHORT;	types[2] = CHAR;
+	types[3] = FLOAT;	 types[4] = DOUBLE;	types[5] = LONG;
+	types[6] = UNSIGNED; types[7] = SIGNED;	types[8] = VOID;
+
+	CurrentCodeLine = (char*)malloc(sizeof(char) * MaxLineLength);
+	if (CurrentCodeLine == NULL) 
+	{
+		printf("Не удалось выделить память под строку из файла\n");
+		return -12;
+	}
+	while (fgets(CurrentCodeLine, MaxLineLength, inputThread) != NULL)
+	{
+		lineLength = GetLineLength(CurrentCodeLine);      
+		for (i = 0; i < countOfTypes; i++) 
+		{
+			index = 0;
+			searchFrom = 0;
+			while ((searchFrom = VariableContains(CurrentCodeLine, lineLength, searchFrom, types[i].VarName, types[i].Length)) >= 0)
+			{
+				VariablesInline = 1;
+				indexPrev = searchFrom - 1;
+				searchFrom += types[i].Length;
+				isFunctionType = false;
+				isModVar = false;
+				index = searchFrom;
+
+				while (CurrentCodeLine[indexPrev] != NULL) 
+				{
+					if (CurrentCodeLine[indexPrev] == ';' || CurrentCodeLine[indexPrev] == NULL) break;
+					if (CurrentCodeLine[indexPrev] == 'g')
+						isModVar = true;
+					indexPrev--;
+				}
+				if (isModVar == false) 
+				{
+					while (CurrentCodeLine[index] != '\0')
+					{
+						if (CurrentCodeLine[index] == ';') break;
+						else if (CurrentCodeLine[index] == '(' || CurrentCodeLine[index] == ')')
+						{
+							isFunctionType = true;
+						}
+						else if (CurrentCodeLine[index] == ',')
+						{
+							VariablesInline++;
+						}
+						index++;
+					}
+					if (isFunctionType == false)
+					{
+						index = searchFrom;
+						printf("\n%d) %s", numberOfVar, types[i].VarName);
+						numberOfVar++;
+						while (CurrentCodeLine[index] != ';' && CurrentCodeLine[index] != '=' && CurrentCodeLine[index] != '\0')
+						{
+							if (CurrentCodeLine[index] == ',')
+							{
+								printf("\n%d) %s ", numberOfVar, types[i].VarName);
+								numberOfVar++;
+							}
+							else printf("%c", CurrentCodeLine[index]);
+							index++;
+						}
+						countOfVariables += VariablesInline;
+					}
+				}
+			}
+		}
+	}
+	printf("\n");
+	return countOfVariables;
 }
--- KR_debugC/test.c
+++ KR_debugC/test.c
@@ -1,30 +1,30 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <locale.h>
+ #define _CRT_SECURE_NO_WARNINGS
+ #include <stdio.h>
+ #include <string.h>
+ #include <locale.h>
 
-int getStringLength(char* string);
+  void function (int a, int n);
 
-typedef struct TextFormat
-{
-	unsigned char  stringLength;
-	unsigned char  countOfLetters;
-	int   sumOfLetterCodes;
-	unsigned char* string;
-
-}Format;
-
-int main(void)
-{
-	int a ;
-	float b;
-	char c;
-	
-	b = 1;
-	a = b * b;
-
-	c  = '1' +
-		 '2' +
-		 '3';
-	return 0;
-}
-
+ int maina()
+ {
+	 int value33;
+	 int a = 1;
+	float value;
+	char* line = "hello world";
+	int g; int lineLength = 0;
+	void* input; int ar = 1;
+ 	unsigned int x = 4;
+	input = NULL;
+	a ^= 5; ar &= 1;
+	a -= 5;
+	a *= 1; a /= 5;
+	a + 3;
+	lineLength = strlen(line);
+	value = (int)1; 
+	value33 = (value=7) * 5;
+	value = ++value33;
+	line  = '1' + '2' + '3'; sin(value=5);
+	value = !value;
+	x++;
+	return x=70;
+ }
